a7473868d6eee95693eb8c1c15a22a9afed73dcb
andz89m <andzrivero89@gmail.com>
2023-09-02 01:40:28 -0800

    add name

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const Header = () => {
   return <>
-  <div className="bg-pink-600 text-white flex items-center justify-between p-2"><div className="text-2xl font-semibold ">Dashboard</div> 
+  <div className="bg-pink-600 text-white flex items-center justify-between p-2"><div className="text-2xl font-semibold ">Dashboard
+  <h6 className="text-lg">Online Fast Food Ordering System</h6>
+  </div> 
   <ul className="flex items-center gap-4">
     
-    <li className="bg-pink-500 p-1 rounded">Logout</li>
+    <li className="bg-pink-500 p-1 rounded hover:cursor-pointer">Logout</li>
     </ul>
     </div></>;
 };
