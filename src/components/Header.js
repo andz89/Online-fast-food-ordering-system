d81322a350dadccaac95ec18d08b56f110fba1c4
andz89m <andzrivero89@gmail.com>
2023-09-02 01:35:33 -0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return <>
+  <div className="bg-pink-600 text-white flex items-center justify-between p-2"><div className="text-2xl font-semibold ">Dashboard</div> 
+  <ul className="flex items-center gap-4">
+    
+    <li className="bg-pink-500 p-1 rounded">Logout</li>
+    </ul>
+    </div></>;
+};
+
+export default Header;
---
+++ src/components/Section.js
@@ -0,0 +1,132 @@
+import React from "react";
+
+const Section = () => {
+  return <>
+  <div className="flex flex-wrap items-center gap-5 m-5">
+  <div class=" p-4 bg-slate-700 w-48 text-white">
+<div class="flex flex-wrap">
+<div class="relative  pr-4   flex-grow flex-1">
+<h5 class="text-blueGray-400 uppercase font-bold text-xs">
+New users
+</h5>
+<span class="font-semibold text-xl  ">
+2,356
+</span>
+</div>
+<div class="relative w-auto pl-4 flex-initial">
+ 
+</div>
+</div>
+<p class="text-sm text-blueGray-400 mt-4">
+<span class=" mr-2">
+<i class="fas fa-arrow-down"></i> 3.48%
+</span>
+<span class="whitespace-nowrap"> Since last week </span>
+</p>
+</div>
+
+<div class=" p-4 bg-slate-700 w-48 text-white">
+<div class="flex flex-wrap">
+<div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+<h5 class="text-blueGray-400 uppercase font-bold text-xs">
+Sales
+</h5>
+<span class="font-semibold text-xl text-blueGray-700">
+924
+</span>
+</div>
+ 
+</div>
+<p class="text-sm text-blueGray-400 mt-4">
+<span class="  mr-2">
+<i class="fas fa-arrow-down"></i> 1.10%
+</span>
+<span class="whitespace-nowrap"> Since yesterday </span>
+</p>
+</div>
+
+<div class=" p-4 bg-slate-700 w-48 text-white">
+<div class="flex flex-wrap">
+<div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+<h5 class="text-blueGray-400 uppercase font-bold text-xs">
+Pending Orders
+</h5>
+<span class="font-semibold text-xl text-blueGray-700">
+45
+</span>
+</div>
+ 
+</div>
+<p class="text-sm text-blueGray-400 mt-4">
+<span class="  mr-2">
+ 
+</span>
+<span class="whitespace-nowrap">   </span>
+</p>
+</div>
+
+<div class=" p-4 bg-slate-700 w-48 text-white">
+<div class="flex flex-wrap">
+<div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+<h5 class="text-blueGray-400 uppercase font-bold text-xs">
+Complete Orders
+</h5>
+<span class="font-semibold text-xl text-blueGray-700">
+134
+</span>
+</div>
+ 
+</div>
+<p class="text-sm text-blueGray-400 mt-4">
+<span class="  mr-2">
+ 
+</span>
+<span class="whitespace-nowrap">   </span>
+</p>
+</div>
+
+<div class=" p-4 bg-slate-700 w-48 text-white">
+<div class="flex flex-wrap">
+<div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+<h5 class="text-blueGray-400 uppercase font-bold text-xs">
+Tasks
+</h5>
+<span class="font-semibold text-xl text-blueGray-700">
+12
+</span>
+</div>
+ 
+</div>
+<p class="text-sm text-blueGray-400 mt-4">
+<span class="  mr-2">
+ 
+</span>
+<span class="whitespace-nowrap">   </span>
+</p>
+</div>
+
+<div class=" p-4 bg-slate-700 w-48 text-white">
+<div class="flex flex-wrap">
+<div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+<h5 class="text-blueGray-400 uppercase font-bold text-xs">
+Issues
+</h5>
+<span class="font-semibold text-xl text-blueGray-700">
+14
+</span>
+</div>
+ 
+</div>
+<p class="text-sm text-blueGray-400 mt-4">
+<span class="  mr-2">
+ 
+</span>
+<span class="whitespace-nowrap">   </span>
+</p>
+</div>
+  </div>
+  
+  </>;
+};
+
+export default Section;
